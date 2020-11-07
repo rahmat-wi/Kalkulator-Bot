4ec80414d796c0e65f4e4ccbc1be4e959f5c7b99
rahmat-wi <rahmat.wibowo.itb@gmail.com>
2020-11-07 11:48:43 +0700

    Upload Kalkulator Bot

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const { LineBot } = require("bottender");
 const { createServer } = require("bottender/express");
+
 const bot = new LineBot({
   // ubah ke access token dan channelSecret dibawah, sesuai dengan yang ada di line console
   accessToken: "XTWOt9SWjP5va2iH0bst9hWyApckB9XeLXEt3rIoWUE4PjnepSCz3RQhO+Djmlx4eqJxxmkPAIa0jorx7wsmg8s/wB4+TGDKLF6ZgodD8bCoTvUKLmAG/hnTak4UBVAC3+KO2QTuya57lCVyLJWmFgdB04t89/1O/w1cDnyilFU=",
   channelSecret: "9dc242b72a51cd1df0fd925f0483b994"
-});
-bot.onEvent(async context => {
+});bot.onEvent(async context => {
   // 1. Pengecekan apakah bot menerima chat berupa text
   if (context.event.isText) {
     // 2. Ambil value text yang dikirim oleh user, simpan di variabel receivedMessage
     const receivedMessage = context.event.text;
+
     // 3. Pengecekan apakah user mengirim 2 pasang string dengan spasi
     // Contoh valid text: 1 3 | 4 2 | 10 23
     if (receivedMessage.split(" ").length === 2) {
       // 4. Menyimpan hasil split. Kalau messagenya: "1 3" splittedText akan berisi ["1", "3"]
       const splittedText = receivedMessage.split(" ");
+
       // 5. Ambil 2 angka yang masih dalam bentuk string, sekaligus ubah menjadi Number (integer)
       const first = Number(splittedText[0]);
       const second = Number(splittedText[1]);
+
       // 6. Lakukan proses penjumlahan
       const sumResult = first + second;
+
       // 7. Balas pesan user dengan hasil penjumlahan 2 angka yang dikirim
       await context.replyText(sumResult);
     } else {
@@ -30,7 +34,9 @@ bot.onEvent(async context => {
     }
   }
 });
+
 const server = createServer(bot);
+
 server.listen(process.env.PORT || 5000, () => {
-  console.log("server is running on 5000 port...");
+  console.log("server is running on port 5000...");
 });
